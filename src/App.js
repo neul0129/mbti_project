7da3e48e90e66a92f1c9af5321b639217cb853f5
hsy <69149030+dev-redo@users.noreply.github.com>
2022-02-04 00:47:52 +0900

    Feat: mbti data 수정
    mbti 데이터를 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
 
 const data = [
+  // ISTJ
   {
-    name: 'ENTP',
+    name: 'INTJ',
     color: '#A8BD5F',
     nickname: '열성적인 중재자',
     description: ' 내적신념이 깊은 정열적인 중재자 유형',
@@ -22,8 +23,9 @@ const data = [
     },
 		image : './profile/image/ENTP.png',
   },
+  // 2. ISFJ
   {
-    name: 'ENTJ',
+    name: 'ISFJ',
     color: '#A4D9D0',
     nickname: '열성적인 중재자',
     description: ' 내적신념이 깊은 정열적인 중재자 유형',
@@ -41,6 +43,7 @@ const data = [
     },
 		image : './profile/image/ENTP.png',
   },
+  // 3. INTJ
   {
     name: 'INTJ',
     color: '#DA7B6B',
@@ -60,6 +63,7 @@ const data = [
     },
 		image : './profile/image/ENTP.png',
   },
+  // 4. INFJ
   {
     name: 'INFJ',
     color: '#BCCB85',
@@ -78,6 +82,246 @@ const data = [
       '안 맞는  MBTI': ["ISFP", "INTP", "INFP"]
     },
 		image : 'src/pages/profile/image/ENFJ.png',
+  },
+  // 5. ISTP
+  {
+    name: 'ISTP',
+    color: '#675887',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 6. ISFP
+  {
+    name: 'ISFP',
+    color: '#A5251C',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 7. INFP
+  {
+    name: 'INFP',
+    color: '#DD803C',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 8. INTP
+  {
+    name: 'INTP',
+    color: '#722569',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 9. ESTP
+  {
+    name: 'ESTP',
+    color: '#0FA9E6',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 10. ESFP
+  {
+    name: 'ESFP',
+    color: '#74BB8E',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 11. ENFP
+  {
+    name: 'ENFP',
+    color: '#D0A942',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 12. ENTP
+  {
+    name: 'ENTP',
+    color: '#176370',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 13. ESTJ
+  {
+    name: 'ESTJ',
+    color: '#ACC043',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 14. ESFJ
+  {
+    name: 'ESFJ',
+    color: '#AE4266',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 15. ENFJ
+  {
+    name: 'ENFJ',
+    color: '#15325A',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
+  },
+  // 16. ENTJ
+  {
+    name: 'ENTJ',
+    color: '#C86042',
+    nickname: '열성적인 중재자',
+    description: ' 내적신념이 깊은 정열적인 중재자 유형',
+    character: "신중한 성격",
+    prosCons: {
+      '장점': ["의지할 수 있다", "인내심이 많다"],
+      '단점': ["부끄러움이 많다", "감정에 억압한다"]
+    },
+    fact: ["원칙주의자", "남 눈치 많이 봄", "남들 챙기는 거 좋아함"],
+    job: ["간호사", "소설가", "예능인"],
+    love: ["상대방에게 답장이 없으면 괜히 잘못보낸게 아닌가 걱정됨", "상대방에게 거의 맞추려고 함"],
+    chemistry: {
+      '잘 맞는  MBTI': ["ENFP", "ESFJ", "ISFP"], 
+      '안 맞는  MBTI': ["ISFP", "INTP", "INFP"],
+    },
+		image : './profile/image/ENTP.png',
   },
 ];
 
