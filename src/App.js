2d7f3423b6e537a031080cf1e55d63c3d1746944
hsy <69149030+dev-redo@users.noreply.github.com>
2022-02-18 17:11:25 +0900

    feat: 경로수정
    각 폴더 index 경로 제거했습니다.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/index";
-import Profile from "./pages/Profile/index";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import "./App.css";
 
 const data = [
