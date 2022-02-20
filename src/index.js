9b7c22daf8308b3dd066cf1677a065be6f239826
hsy <69149030+dev-redo@users.noreply.github.com>
2022-02-20 13:41:41 +0900

    Feat: 깃허브 경로 수정
    깃허브 페이지 경로 수정했습니다.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+  </HashRouter>,
+  document.getElementById('root'),
+)
