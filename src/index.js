eb8e703c9af2d91648c65ae6f5848463ad912f23
hsy <69149030+dev-redo@users.noreply.github.com>
2022-02-26 23:57:10 +0900

    Fix: router 수정
    Router 옵션을 수정했습니다.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter basename="/mbti_project">
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+)
